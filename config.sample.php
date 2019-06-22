84b8f43358e590882ff3f5309d4adde2a60d65c6
Gabriel Roch <gabriel@g-roch.ch>
2019-06-22 15:37:30 +0200

    Correction in config.sample and add webmaster user

--- config.sample.php
+++ config.sample.php
@@ -4,10 +4,14 @@ mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
 
 // @TODO complete
-// const DB_DSN = "mysql:dbname=myDB;charset=utf8mb4";
-//const DB_USER = "myUser";
-//const DB_PW = "myPwd";
+// const DB_DSN = 'mysql:dbname=myDB;charset=utf8mb4';
+//const DB_USER = 'myUser';
+//const DB_PW = 'myPwd';
 const DB_OPT = [];
+
+// Webmaster user need to correspond to a user of DB. 
+//const WEBMASTER_USER = 'webmaster';
+//define('WEBMASTER_PW', password_hash(bin2hex(random_bytes(30)), PASSWORD_DEFAULT));
 
 
 const CONF_LOADED = true; 
