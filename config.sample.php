7c8ddcddbadbb536d7ae4b4b638df155412059d5
Gabriel Roch <gabriel@g-roch.ch>
2019-06-22 13:22:41 +0200

    Add config

---
+++ config.sample.php
@@ -0,0 +1,14 @@
+<?php
+
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+
+// @TODO complete
+// const DB_DSN = "mysql:dbname=myDB;charset=utf8mb4";
+//const DB_USER = "myUser";
+//const DB_PW = "myPwd";
+const DB_OPT = [];
+
+
+const CONF_LOADED = true; 
+
---
+++ www/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once '../config.php';
+
+
+var_dump(new PDO(DB_DSN, DB_USER, DB_PW, DB_OPT));
